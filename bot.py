f9b54cba598508e3c8bbac46882580d3aeb5a48e
Rokas <rbomblauskas@gmail.com>
2024-09-27 00:01:56 +0300

    Firebase

--- bot.py
+++ bot.py
@@ -1,13 +1,30 @@
 import discord
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
+
+# Use a service account.
+cred = credentials.Certificate('mif-renginys-firebase-adminsdk-ld2k1-dbbfebe5f1.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client() # Maybe we will need to use async client
 
 bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    print(f'We have logged in as {bot.user}')
 
 @bot.slash_command(guild_ids=[1288951632200990881])
 async def hello(ctx):
-    await ctx.respond("Hello!")
+    await ctx.respond('hi')
+
+@bot.slash_command(guild_ids=[1288951632200990881])
+async def get_user_by_name(ctx, name: str):
+    user_ref = db.collection('users').where(filter=FieldFilter('dc_username', '==', name)).limit(1).stream()
+    user_data = None
+    for doc in user_ref:
+        user_data = doc.to_dict()
+    await ctx.respond(f'{user_data}')
 
 bot.run("MTI4ODk1MTgyMTkxNzg4NDQ0Ng.GJp8HR.AhbEBj7XgP5YDu_jV7ngeOM4xJilbEPMFJfQRM")
